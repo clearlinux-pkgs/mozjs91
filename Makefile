8f944bc213ab0945a848ae306564e282e91945d8
Patrick McCarty <patrick.mccarty@intel.com>
2021-08-13 17:44:46 -0700

    Initial import of mozjs91
    Noteworthy packaging changes compared to mozjs78:
    
    - Ported to the new python3-based build.
    
    - Modified init_patch.patch and spidermonkey_checks_disable.patch to
      print diagnostic messages instead of commenting out the code.
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

---
+++ Makefile
@@ -0,0 +1,6 @@
+PKG_NAME := mozjs91
+
+include ../common/Makefile.common
+
+update:
+	./update.sh
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+PKG=mozjs91
+
+git pull --ff-only
+VERSION=$(curl -sSf https://archive.mozilla.org/pub/firefox/releases/ \
+	    | grep href \
+	    | cut -f3 -d">" \
+	    | cut -f1 -d"/" \
+	    | sed -nr 's/^(91\.[0-9.]+)esr/\1/p' \
+	    | sort --version-sort \
+	    | tail -1)
+
+if [[ -z "${VERSION}" ]]; then
+    echo "Unable to find version upstream."
+    exit 1
+fi
+
+CURRENT_VERSION=$(grep "^Version" $PKG.spec | awk '{ print $3 }')
+CURRENT_RELEASE=$(grep "^Release" $PKG.spec | awk '{ print $3 }')
+
+if [[ v"${CURRENT_VERSION}" == v"${VERSION}" ]]; then
+	exit 2
+fi
+
+sed -e "s/##VERSION##/${VERSION}/g;s/##RELEASE##/${CURRENT_RELEASE}/g" $PKG.spec.in > $PKG.spec
+
+make generateupstream || exit 3
+
+make bumpnogit
+git add $PKG.spec Makefile release upstream
+git commit -s -m "Update to ${VERSION}"
+make koji
